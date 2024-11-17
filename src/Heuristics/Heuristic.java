c94c09d8fe2396522f663329560b5b3c4483b946
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2024-11-17 15:53:48 +0600

    Delete src directory

--- src/Heuristics/Heuristic.java
+++
@@ -1,9 +0,0 @@
-package Heuristics;
-import Board.MancalaBoard;
-import java.util.Random;
-
-public abstract class Heuristic {
-  public abstract int getHeuristicValue(MancalaBoard board);
-  int range1 = 7;
-  int range2 = 13;
-}
